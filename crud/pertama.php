b468269342d9cdde06f176c0b5ab22595895a2ca
Harniyati <90300643+Harniyati@users.noreply.github.com>
2021-12-23 12:03:33 +0700

    Add files via upload

---
+++ crud/pertama.php
@@ -0,0 +1,53 @@
+<body style="background-image:url(upik.jpg)" >
+    
+<?php
+echo "<center>";
+echo "<h1>DATA DIRI</h1>";
+echo "<center>";
+echo "<table = 400 widht =200px height = 400px border =1> ";
+
+echo "<tr>";
+echo "<td style = text-align:center colspan = 4 >BIODATA SAYA </td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>NAMA</td>";
+echo "<td>:</td>";
+echo "<td>HARNIYATI</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>TEMPAT,TANGGAL LAHIR</td>";
+echo "<td>:</td>";
+echo "<td>REO, 28 DESEMBER 2001</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>NIM</td>";
+echo "<td>:</td>";
+echo "<td>19024014075</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>KELAS</td>";
+echo "<td>:</td>";
+echo "<td>A2</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>JURUSAN</td>";
+echo "<td>:</td>";
+echo "<td>TEKNIK INFORMATIKA</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>HOBI</td>";
+echo "<td>:</td>";
+echo "<td>MENYANYI</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>ALAMAT</td>";
+echo "<td>:</td>";
+echo "<td>PERINTIS KEMERDEKAAN VII</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<img width=130px height=150pxpx src='harny.jpg'>";
+echo "</tr>";
+
+echo "</table>";
+echo "</center>";
+?>
